044c93b9d42e3519918275eb610e55049231b1df
Teatree <Teatree@BlackLabel>
2014-04-01 23:12:09 +0300

    attempt #1

--- SwampMonster/src/com/me/swampmonster/GUI/SlotMachine.java
+++ SwampMonster/src/com/me/swampmonster/GUI/SlotMachine.java
@@ -4,22 +4,46 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.me.swampmonster.models.AbstractGameObject;
-import com.me.swampmonster.models.Toughness;
 import com.me.swampmonster.pickable.Perks;
 
 public class SlotMachine extends AbstractGameObject{
 	
 	boolean slotMachine;
+	private int timer;
 	
+	private Animation animation;
 	private Random random;
 	private Map <Integer, Perks> perkParams;
+	private Texture texture;
+	private TextureRegion[] frames;
+	private TextureRegion currentFrame;
 	
 	public SlotMachine(){
 		slotMachine = false;
-		sprite = new Sprite(new Texture("data/slotMachineCase.png"));
+		timer = 0;
+		texture = new Texture(Gdx.files.internal("data/perkSheet.png"));
+		sprite = new Sprite(texture);
+		TextureRegion[][] tmp = TextureRegion.split(texture, texture.getWidth() / 8,
+						texture.getHeight() / 8);
+		frames = new TextureRegion[8 * 8];
+
+		int index = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				frames[index++] = tmp[i][j];
+			}
+		}
+		animation = new Animation(1, frames);
+		
+		currentFrame = animation.getKeyFrame(0);
+		
+		sprite = new Sprite(currentFrame);
 		sprite.setX(275);
 		sprite.setY(175);
 		sprite.setScale(2, 2);
@@ -42,7 +66,21 @@ public class SlotMachine extends AbstractGameObject{
 	}
 	
 	public void update(){
-		
+		if(slotMachine){
+			if(timer<60){
+				int perkRand = random.nextInt();
+				
+				Perks perks = null;
+				perks = perkParams.get((perkRand));
+				
+				currentFrame = animation.getKeyFrame(perkRand);
+				sprite = new Sprite(currentFrame);
+				
+				timer++;
+			}
+		}else{
+			timer=0;
+		}
 	}
 
 	public boolean isSlotMachine() {
